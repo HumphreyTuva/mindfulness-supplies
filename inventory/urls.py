28f99dd5167ee2ed94db6ef34649fb09b631c316
Fikiri20 <humphreytuva2001@gmail.com>
2026-06-26 13:36:07 +0300

    Remove unwanted root files

--- inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import JsonResponse
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lambda request: JsonResponse({"message": "Welcome to the Inventory API"})),  # Optional welcome message
-    path('api/', include('inventory_app.urls')),  # All your inventory API endpoints
-]
